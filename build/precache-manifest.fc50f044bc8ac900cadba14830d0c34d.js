a3228fcd5a0d6b7524f0ceb8b6ee0f543863b04e
lauffenp <lauffenp@gmail.com>
2018-12-10 21:09:18 -0800

    Initial commit

---
+++ build/precache-manifest.fc50f044bc8ac900cadba14830d0c34d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "faaafe74b38d06901582",
+    "url": "/static/js/main.faaafe74.chunk.js"
+  },
+  {
+    "revision": "be7b1f34f2da9bc1f117",
+    "url": "/static/js/1.be7b1f34.chunk.js"
+  },
+  {
+    "revision": "faaafe74b38d06901582",
+    "url": "/static/css/main.9c2c9097.chunk.css"
+  },
+  {
+    "revision": "44d0855170f716b242842177c8aee259",
+    "url": "/index.html"
+  }
+];
