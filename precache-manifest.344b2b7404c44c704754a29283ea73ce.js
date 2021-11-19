41214bc75eed4dd78eebec665cea2875399fece6
lauffenp <lauffenp@gmail.com>
2021-11-19 19:16:04 +0100

    update image editor per Alison's requests

---
+++ precache-manifest.344b2b7404c44c704754a29283ea73ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7d93dcc9fc7058567f4",
+    "url": "/static/css/main.39f2b668.chunk.css"
+  },
+  {
+    "revision": "a7d93dcc9fc7058567f4",
+    "url": "/static/js/main.a7d93dcc.chunk.js"
+  },
+  {
+    "revision": "e8eae8bbeee1ede3ef20",
+    "url": "/static/css/1.a1d4356b.chunk.css"
+  },
+  {
+    "revision": "e8eae8bbeee1ede3ef20",
+    "url": "/static/js/1.e8eae8bb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "345aa61858767875e16481deefe6a096",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aef242465a029755bad23cd2456d779f.js"
+  "/precache-manifest.344b2b7404c44c704754a29283ea73ce.js"
 );
 
 workbox.clientsClaim();
